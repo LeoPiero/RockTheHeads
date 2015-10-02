0449bc31edc92b92af4b1f58698e2ee3d98b2812
Jason Ridenour <davincilab@gmail.com>
2015-10-02 09:03:41 -0400

    Added Asynch

--- commandScript/commandScript.sh
+++ commandScript/commandScript.sh
@@ -3,3 +3,5 @@ screen -S minecraft -p 0 -X stuff "`printf "eco give LeonardoPiero 5 \r"`";
 screen -S minecraft -p 0 -X stuff "`printf "eco give LeonardoPiero 5 \r"`";
 screen -S minecraft -p 0 -X stuff "`printf "eco give LeonardoPiero 5 \r"`";
 screen -S minecraft -p 0 -X stuff "`printf "eco give LeonardoPiero 5 \r"`";
+screen -S minecraft -p 0 -X stuff "`printf "eco give LeonardoPiero 5 \r"`";
+screen -S minecraft -p 0 -X stuff "`printf "eco give LeonardoPiero 5 \r"`";
--- widgets/footer.php
+++ widgets/footer.php
@@ -14,11 +14,11 @@
             </footer>
         </div>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" async></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" async></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-        <script src="js/main.js"></script>
+        <script src="js/main.js" async></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
