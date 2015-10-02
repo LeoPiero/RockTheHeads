608b6edad8aedee36c5671da171ce0267ef602a2
Jason Ridenour <davincilab@gmail.com>
2015-10-02 14:21:07 -0400

    Final first version push

---
+++ minifyStuff.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Minify js/css files using csstidy and jsmin command line tools
+# Original files must be in a subfolder called src (you can change this).
+# Generate files are saved in the parent folder.
+# Example: "css/src/style.css" will be saved as "css/style.css"
+# TODO: add "blacklist" and possibility to combine multiple files in one
+
+# css path
+csspath='css/src/*'
+
+# js path
+jspath='js/src/*'
+
+for file in $csspath
+do
+    #echo 'css'
+    srcdir=$(dirname ${file})
+    scrfile=$srcdir'/../'$(basename $file)
+    yui-compressor --type css -o $scrfile $file
+
+    # # add new file to commit
+    git add $scrfile
+done
+
+
+for file in $jspath
+do
+    srcdir=$(dirname ${file})
+    scrfile=$srcdir'/../'$(basename $file)
+	yui-compressor --type js -o $scrfile $file
+
+    # add new file to commit
+    git add $scrfile
+done
